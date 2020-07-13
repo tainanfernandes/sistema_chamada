a7a02a7517469f9f4c64dddfeae1fbd599f0a207
pipowss <pipowss@users.noreply.github.com>
2020-07-13 17:36:00 -0300

    Validação Javscript

--- js/novousuario.js
+++ js/novousuario.js
@@ -1,19 +1,137 @@
-$('#radioProfessor').click(_ => {
-    let ra = $('#inputRaWrap'), prof = $('#inputCodProfessorWrap')
+// Mascara do CPF
+$('#inputCpf').inputmask('999.999.999-99', {clearIncomplete: true, removeMaskOnSubmit: true})
 
-    ra.addClass('d-none')
-    ra.children('input').removeAttr('required')
+// Validador do CPF
+$.validator.addMethod('cpf', (value, element) => {
+    value = value.replace(/\./g, '')
+    value = value.replace(/\-/g, '')
+    value = value.replace(/\s/g, '').trim()
 
-    prof.removeClass('d-none')
-    prof.children('input').attr('required', true)
+    if (/^[0-9]+$/.test(value) && value.length == 11) {
+        let first_dig = 0
+        let second_dig = 0
+        let streak = value[0]
+
+        // Soma os valores dos 9 promeiros digitos e multiplica de acordo com a formula
+        for (var i = 0; i < 9; i++) {
+            first_dig += parseInt(value[i]) * (10 - i)
+            second_dig += parseInt(value[i]) * (11 - i)
+
+            // Verifica por CPF com todos os numeros repetidos
+            if (value[i] == streak) {
+                streak = value[i]
+            }
+            else streak = null
+        }
+
+        // Se todods os numeros forem iguais
+        if (streak !== null && streak == value[9] && streak == value[10]) {
+            return false
+        }
+
+        // Valida o primeiro digito verificador
+        first_dig = (first_dig * 10) % 11
+        if (first_dig == 10) first_dig = 0
+        if (first_dig != parseInt(value[9])) {
+            return false
+        }
+
+        // Valida o segundo digito verificador
+        second_dig += first_dig * 2
+        second_dig = (second_dig * 10) % 11
+        if (second_dig == 10) second_dig = 0
+        if (second_dig != parseInt(value[10])) {
+            return false
+        }
+
+        return true
+    }
+    else return false
+}, 'Por favor insira um CPF válido.')
+
+// Validador senha forte
+$.validator.addMethod('senhaForte', (value, element) => {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!-/:-@\[-`{-~])(?=.{8,})/g.test(value)
+}, 'Insira uma senha forte.')
+
+// Validador
+$('#formCadastrar').validate({
+    errorElement: 'div',
+    errorClass: 'invalid-feedback',
+    highlight: (element, errorClass) => {
+        $(element).removeClass('is-valid')
+        $(element).addClass('is-invalid')
+    },
+    unhighlight: (element, errorClass) => {
+        $(element).addClass('is-valid')
+        $(element).removeClass('is-invalid')
+    },
+    onfocusout: (element) => {
+        $(element).valid()
+        $(element).data('validated', true)
+    },
+    onkeyup: (element) => {
+        if ($(element).data('validated')) {
+            $(element).valid()
+        }
+    },
+
+    rules: {
+        inputNome: 'required',
+        inputSobrenome: 'required',
+        inputEmail: {
+            required: true,
+            email: true
+        },
+        inputCpf: {
+            required: true,
+            cpf: true
+        },
+        inputPassword: {
+            required: true,
+            senhaForte: true
+        },
+        inputPasswordRepeat: {
+            required: true,
+            equalTo: '#inputPassword'
+        }
+    },
+    messages: {
+        inputNome: 'Este campo é obrigatório.',
+        inputSobrenome:  'Este campo é obrigatório.',
+        inputEmail: {
+            required: 'Este campo é obrigatório.',
+            email: 'Por favor insira um endereço de e-mail válido.'
+        },
+        inputCpf: {
+            required: 'Este campo é obrigatório.'
+        },
+        inputPassword: {
+            required: 'Este campo é obrigatório.'
+        },
+        inputPasswordRepeat: {
+            required: 'Este campo é obrigatório.',
+            equalTo: 'As duas senhas devem corresponder.'
+        },
+        inputRa:  'Este campo é obrigatório.',
+        inputCodProfessor:  'Este campo é obrigatório.'
+    }
 })
 
+// Trocar pra 'Sou Professor'
+$('#radioProfessor').click(_ => {
+    $('#inputRaWrap').addClass('d-none')
+    $('#inputCodProfessorWrap').removeClass('d-none')
+
+    $('#inputRa').rules('remove', 'required')
+    $('#inputCodProfessor').rules('add', 'required')
+})
+
+// Trocar pra 'Sou Aluno'
 $('#radioAluno').click(_ => {
-    let ra = $('#inputRaWrap'), prof = $('#inputCodProfessorWrap')
+    $('#inputRaWrap').removeClass('d-none')
+    $('#inputCodProfessorWrap').addClass('d-none')
 
-    ra.removeClass('d-none')
-    ra.children('input').attr('required', true)
-
-    prof.addClass('d-none')
-    prof.children('input').removeAttr('required')
+    $('#inputRa').rules('add', 'required')
+    $('#inputCodProfessor').rules('remove', 'required')
 })
