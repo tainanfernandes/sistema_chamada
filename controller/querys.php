fe27d1a963d4f05bec3145727280b52db40b4120
HerlissonJordane <h.jmoreira@hotmail.com>
2020-09-28 19:22:31 -0300

    Modificação atributo consulta
    Mudança do atributo de chamada para Pin da turma

--- controller/querys.php
+++ controller/querys.php
@@ -2,7 +2,7 @@
 require_once '../controller/mysqli_connect.php';
 
 function query_materias_aluno($id){
-        $consulta = "SELECT A.NOME AS NOMEALUNO, C.NOME AS NOMECURSO, D.NOME AS NOMEDISCIPLINA, T.DIA AS DIASEMANA, TN.NOME AS TURNO, T.ID AS IDCURSO FROM aluno as A INNER JOIN curso AS C ON A.idcurso = C.id
+        $consulta = "SELECT A.NOME AS NOMEALUNO, C.NOME AS NOMECURSO, D.NOME AS NOMEDISCIPLINA, T.DIA AS DIASEMANA, TN.NOME AS TURNO, T.PIN AS IDCURSO FROM aluno as A INNER JOIN curso AS C ON A.idcurso = C.id
         INNER JOIN disciplina AS D ON C.id = D.idcurso
         INNER JOIN turma AS T ON D.id = T.iddisciplina
         INNER JOIN turno AS TN ON T.idturno = TN.id
