0b39cc5ba417faf53a01bb69f63cbd35d4005685
Bruno Diego <brunodiego97@gmail.com>
2020-09-30 20:41:31 -0300

    Criação da query do accordion do professor

--- controller/querys.php
+++ controller/querys.php
@@ -1,8 +1,9 @@
 <?php
 require_once '../controller/mysqli_connect.php';
 
-function query_materias_aluno($id){
-        $consulta = "SELECT A.NOME AS NOMEALUNO, C.NOME AS NOMECURSO, D.NOME AS NOMEDISCIPLINA, T.DIA AS DIASEMANA, TN.NOME AS TURNO, T.PIN AS IDCURSO FROM aluno as A INNER JOIN curso AS C ON A.idcurso = C.id
+function query_materias_aluno($id)
+{
+    $consulta = "SELECT A.NOME AS NOMEALUNO, C.NOME AS NOMECURSO, D.NOME AS NOMEDISCIPLINA, T.DIA AS DIASEMANA, TN.NOME AS TURNO, T.PIN AS IDCURSO FROM aluno as A INNER JOIN curso AS C ON A.idcurso = C.id
         INNER JOIN disciplina AS D ON C.id = D.idcurso
         INNER JOIN turma AS T ON D.id = T.iddisciplina
         INNER JOIN turno AS TN ON T.idturno = TN.id
@@ -10,3 +11,10 @@ function query_materias_aluno($id){
 
     return $consulta;
 }
+
+$sqlPrincipalProfAccordion =
+    "SELECT di.nome AS NMDISCIPLINA, cs.nome AS NMCURSO, tm.dia AS DIA, tn.nome AS TURNO, tm.pin AS PIN, tm.obsTurma AS OBSERVACAO
+       FROM turno tn, turma tm, disciplina di, curso cs
+      WHERE tm.idturno = tn.id
+        AND tm.iddisciplina = di.id
+        AND di.idcurso = cs.id";
